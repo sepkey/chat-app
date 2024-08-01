4212dc66962da2c30e6834c0e0f063fe9c0708d3
Sepide Kia <sepidekia6990@gmail.com>
2024-08-01 15:39:38 +0330

    ft: create users model

---
+++ src/models/user.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    clerkUserId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    userName: { type: String, required: true, unique: true },
+    email: { type: String },
+    profilePicture: { type: String, required: false },
+    bio: { type: String, required: false },
+  },
+  { timestamps: true }
+);
+
+if (mongoose.models && mongoose.models["users"]) {
+  mongoose.deleteModel("users");
+}
+
+const UserModel = mongoose.model("users", userSchema);
+export default UserModel;
