4ced418527661e8950e9febc5555de1007f5654f
Sepide Kia <sepidekia6990@gmail.com>
2024-08-02 17:38:55 +0330

    ft: add redux toolkit and create the store of user

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,24 @@
+import { IUser } from "@/interfaces/user.interface";
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: { currentUserData: null, currentUserId: "" },
+  reducers: {
+    setCurrentUserData: (state, action) => {
+      state.currentUserData = action.payload;
+    },
+
+    setCurrentUserId: (state, action) => {
+      state.currentUserId = action.payload;
+    },
+  },
+});
+
+export const { setCurrentUserData, setCurrentUserId } = userSlice.actions;
+export default userSlice;
+
+export interface IUserState {
+  currentUserData: IUser | null;
+  currentUserId: string;
+}
