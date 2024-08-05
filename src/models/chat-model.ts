868331451a49acd964a46362458fe088f92b780d
Sepide Kia <sepidekia6990@gmail.com>
2024-08-05 12:26:37 +0330

    ft: create chat model schema

---
+++ src/models/chat-model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    users: { type: [mongoose.Schema.Types.ObjectId], ref: "users" },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "messages",
+    },
+    isGroupChat: { type: Boolean, default: false },
+    groupName: {
+      type: String,
+      default: "",
+    },
+    groupProfilePicture: {
+      type: String,
+      default: "",
+    },
+    groupBio: {
+      type: String,
+      default: "",
+    },
+    groupAdmins: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "users",
+    },
+  },
+  { timestamps: true }
+);
+
+if (mongoose.models && mongoose.models["chats"]) {
+  mongoose.deleteModel("chats");
+}
+
+const ChatModel = mongoose.model("chats", chatSchema);
+export default ChatModel;
