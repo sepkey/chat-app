ef82116954ddf57edea6b7c3a1095f45957e6be8
Sepide Kia <sepidekia6990@gmail.com>
2024-08-12 20:45:41 +0330

    ft: create server action for all the chats

--- src/server-actions/chats.ts
+++ src/server-actions/chats.ts
@@ -9,3 +9,14 @@ export const CreateNewChat = async (payload: unknown) => {
     return { error: err.message };
   }
 };
+
+export const GetAllChats = async (userId: string) => {
+  try {
+    const chats = await ChatModel.find({ users: { $in: [userId] } }).populate(
+      "users"
+    );
+    return JSON.parse(JSON.stringify(chats));
+  } catch (err: any) {
+    return { error: err.message };
+  }
+};
