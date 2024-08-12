2bbbc2212a07a8a3000c2e862d301b4ee47f829d
Sepide Kia <sepidekia6990@gmail.com>
2024-08-12 20:44:16 +0330

    ft: create chats redux store

---
+++ src/redux/chatSlice.ts
@@ -0,0 +1,19 @@
+import { IChat } from "@/interfaces";
+import { createSlice } from "@reduxjs/toolkit";
+
+const chatSlice = createSlice({
+  name: "chat",
+  initialState: { chats: [] },
+  reducers: {
+    setChats: (state, action) => {
+      state.chats = action.payload;
+    },
+  },
+});
+
+export const { setChats } = chatSlice.actions;
+export default chatSlice;
+
+export interface IChatState {
+  chats: IChat[];
+}
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import chatSlice from "./chatSlice";
 
-const store = configureStore({ reducer: { user: userSlice.reducer } });
+const store = configureStore({
+  reducer: { user: userSlice.reducer, chat: chatSlice.reducer },
+});
 
 export default store;
